70aa33e88dc05adf85f4c10a68017a2c895bd91c
ingrid <fermingrid@gmail.com>
2017-01-24 18:01:21 +0100

    Esercizio 4

--- ExcerciseOrderedArray/ExcerciseOrderedArray/ViewController.m
+++ ExcerciseOrderedArray/ExcerciseOrderedArray/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController () {
-    NSArray *_myNumberArray;
+    NSMutableArray *_myNumberArray;
 }
 
 @end
@@ -19,12 +19,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    _myNumberArray = init();
+    _myNumberArray = @[].mutableCopy;
     
     for(int i = 0; i <100; i++){
-        
-        _myNumberArray[i]  = [self getRandomNumberBetween:0 to:100];
+        [_myNumberArray addObject: @(arc4random())];
     }
+    
+    [self.sumArrayLabel setText:[NSString stringWithFormat:@"%i", [self isSumArrayLabel]]];
+    
+    [self ascendentingOrder];
+    
+    [self descendentingSorting];
 }
 
 
@@ -37,12 +42,47 @@
     return (int)from + arc4random() % (to-from+1);
 }
 
--(NSArray *) createRandomNumberArray {
-    
+-(int)isSumArrayLabel{
+    int sum = 0;
+    for(int  i = 0; i < _myNumberArray.count; i ++){
+        sum += [_myNumberArray[i] integerValue];
+    }
+    NSLog(@"La somma dell'Array è: %i", sum);
+    return sum;
+}
 
+-(void)ascendentingOrder{
+    NSArray *sortedArray = [_myNumberArray sortedArrayUsingComparator:^NSComparisonResult(NSNumber *obj1, NSNumber *obj2) {
+        int value1 = obj1.intValue;
+        int value2 = obj2.intValue;
+        
+        if(value1 == value2){
+            return NSOrderedSame;
+        }
+        if (value1<value2){
+            return NSOrderedAscending;
+        }
+        return NSOrderedDescending;
+    }];
     
+    NSLog(@"In ordine ascendente: \n%@", sortedArray);
+}
+
+-(void)descendentingSorting {
+    NSArray *sortedArray = [_myNumberArray sortedArrayUsingComparator:^NSComparisonResult(NSNumber *obj1, NSNumber *obj2) {
+        int value1 = obj1.intValue;
+        int value2 = obj2.intValue;
+        
+        if(value1 == value2){
+            return NSOrderedSame;
+        }
+        if (value1>value2){
+            return NSOrderedAscending;
+        }
+        return NSOrderedDescending;
+    }];
     
-    return _myNumberArray;
+    NSLog(@"In ordine discendente: \n%@", sortedArray);
 }
 
 
